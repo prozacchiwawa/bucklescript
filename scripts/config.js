79833c812805f811f7f02cce200eaa94ee67e282
arty <art.yerkes@gmail.com>
2018-08-30 08:16:31 -0700

    DragonflyBSD in config.js

--- scripts/config.js
+++ scripts/config.js
@@ -3,13 +3,14 @@ var os = require('os')
 var os_type = os.type()
 var os_arch = os.arch()
 var is_windows = !(os_type.indexOf('Windows') < 0)
-var is_bsd = !(os_type.indexOf('BSD') < 0)
+var is_bsd = !(os_type.indexOf('BSD') < 0) || os_type == 'DragonFly'
 
 exports.is_windows = is_windows
 var sys_extension;
 switch (os.type()) {
     case 'Darwin':
     case 'FreeBSD':
+    case 'DragonFly':
     case 'Linux':        
     case 'Windows_NT':
         sys_extension = "." + os.platform(); break;
